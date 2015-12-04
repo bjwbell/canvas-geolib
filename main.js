b95bbe9ac50ac3d6c603ab31e905c0a9c6584d16
Bryan Bell <bryan.w.bell@gmail.com>
2015-12-04 01:40:39 -0800

    main.js - remove commented out debugging code, misc cleanup

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function Vector(x, y){
   };
   this.scalarMult = function(scalar){
 	  return new Vector(this.x * scalar, this.y * scalar);
-  }
+  };
   this.dot = function(v2) {
     return this.x * v2.x + this.y * v2.y;
   };
@@ -59,28 +59,22 @@ function Vector(x, y){
   };
   this.add = function(v2) {
 	  return new Vector(this.x + v2.x, this.y + v2.y);
-  }
+  };
 }
 
 function Segment(p1, p2){
-  //alert('s1');
   this.p1 = p1;
   this.p2 = p2;
-  //alert('s2');
   this.draw = function() {
-    //alert('s3');
     var canvas = getCanvas();
     context = canvas.getContext('2d');
     context.strokeStyle = '#000'; //black
     context.lineWidth = 4;
-    //alert('s4');
     context.beginPath();
     context.moveTo(p1.x, p1.y);
     context.lineTo(p2.x, p2.y);
-    //alert('s5');
     context.closePath();
     context.stroke();
-    //alert('s6');
   };
 
 }
@@ -94,7 +88,7 @@ function cross(v1, v2) {
 /* 
 determines if the 3 points, p1, p2, p3 form a strictly right turn (i.e. they can't be collinear)
 */
-function rightTurn(var p1, var p2, var p3)
+function rightTurn(p1, p2, p3)
 {
 	var v1 = p2.subtract(p1);
 	var v2 = p3.subtract(p2);
@@ -107,12 +101,9 @@ function rightTurn(var p1, var p2, var p3)
 
 
 /*
-*/
-
-/*
 determines if the 3 points, p1, p2, p3 form a strictly left turn (i.e. they can't be collinear)
 */
-function leftTurn(var p1, var p2, var p3){
+function leftTurn(p1, p2, p3){
 	var v1 = p2.subtract(p1);
 	var v2 = p3.subtract(p2);
 	//returns true of the vector from p2 to p3 is a left turn compared to the vector from p1 to p2.
@@ -139,7 +130,6 @@ function leftTurn(var p1, var p2, var p3){
 	(q - p) x r = 0 then the lines are co-linear.
 	
 */
-
 //determine if segments [x1, x2] and [x3, x4] intersect
 function intersect1d(x1, x2, x3, x4){	
 	if((x2 >= x3 || x2 >= x4) && (x1 <= x3 || x1 <= x4)){
@@ -204,32 +194,16 @@ function drawLine2(){
 var debug = 1;
 
 function mouseMove(ev) {
-  //alert('t3');
   var canvas = getCanvas();
   context = canvas.getContext('2d');
   context.fillStyle = '#fff';
   context.fillRect(0, 0, width, height);
   context.fillStyle = '#f00'; //red
-  //alert('t3.5');
   context.strokeStyle = '#000'; //green
   context.lineWidth = 4;
-  //context.beginPath();
-  //alert('t3.75');
-  //context.moveTo(0, 0);
-  //alert('t3.95');
   var x = getEventXCoord(ev);
   var y = getEventYCoord(ev);
-  //context.lineTo(x, y);
-  //alert('t4');
-  //context.closePath();
-  //alert('t5');
-  //context.fill();
-  //alert('t6');
-  //context.stroke();
-  //alert('t7');
-  //alert('b1');
   var seg1 = new Segment(new Vector(0, 0), new Vector(x, y));
-  //alert('b2');
   seg1.draw();
 
   var seg2 = new Segment(new Vector(0, 100), new Vector(100, 50));
@@ -239,6 +213,6 @@ function mouseMove(ev) {
 	  intersectionPoint.color = '#f00';
 	  intersectionPoint.draw();
   }
-};
+}
 
 
